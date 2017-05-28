c0e41f30ec06ad560004b74c0cac750e0b4cb0b5
Artem Altukhov <altukhov.artem.dev@gmail.com>
2017-05-28 23:41:55 +0300

    first commit

---
+++ TextAnalyzer/TextAnalyzer/TextAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalyzer
+{
+    interface TextAnalyzer
+    {
+        Label ProcessText(String text);
+    }
+
+    enum Label
+    {
+        SPAM, NEGATIVE_TEXT, TOO_LONG, OK
+    }
+
+}
